c41fecd896e6c2dd91bcef9eb1be44b8be13faa6
takayuki-acu.mox <taka.harikyu@gmail.com>
2022-05-24 23:58:50 +0900

    治療内容投稿実装

--- app/controllers/therapies_controller.rb
+++ app/controllers/therapies_controller.rb
@@ -2,6 +2,25 @@ class TherapiesController < ApplicationController
 
 
   def index
-   
+    @therapies = Therapy.all
+  end
+
+  def new
+    @therapy = Therapy.new
+  end
+
+  def create
+    @therapy = Therapy.new(therapy_params)
+    if @therapy.save
+      redirect_to root_path(@therapy)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def therapy_params
+    params.require(:therapy).permit(:title, :text, :category_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/therapy.rb
+++ app/models/therapy.rb
@@ -1,8 +1,11 @@
 class Therapy < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category
+  has_one_attached :image
 
   validates :title, :text, presence: true
-
+  validates :text, :text, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :image, presence: true
 end

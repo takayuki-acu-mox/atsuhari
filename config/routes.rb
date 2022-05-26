671bd8de1898a4603f40daf204e693970ff8a5d3
takayuki-acu.mox <taka.harikyu@gmail.com>
2022-05-26 13:43:09 +0900

    検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   resources :therapies, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
 end
